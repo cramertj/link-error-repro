af4f13e1751af47cf5876ba3ca80c6aca62d2463
Taylor Cramer <cramertj@google.com>
2018-09-06 16:58:59 -0700

    Pin versions

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-CARGO_TARGET_LINKER=./clang/bin/clang \
-CARGO_TARGET_RUSTFLAGS="-Clink-arg=--target=x86_64-fuchsia -Clink-arg=--sysroot=./sdk/arch/x64/sysroot -Lnative=./sdk/arch/x64/lib" \
-cargo build -v --target=x86_64-fuchsia
+CARGO_INCREMENTAL=0 \
+CARGO_TARGET_X86_64_FUCHSIA_LINKER=./clang/bin/clang \
+CARGO_TARGET_X86_64_FUCHSIA_RUSTFLAGS="-Clink-arg=--target=x86_64-fuchsia -Copt-level=z -Clink-arg=--sysroot=./sdk/arch/x64/sysroot -Lnative=./sdk/arch/x64/lib" \
+CC=./clang/bin/clang \
+CFLAGS="--sysroot=./sdk/arch/x64/sysroot -L ./sdk/arch/x64/lib" \
+cargo build -v --target=x86_64-fuchsia --release
--- clone_sdk.sh
+++ clone_sdk.sh
@@ -2,5 +2,5 @@
 
 curl -L -o cipd "https://chrome-infra-packages.appspot.com/client?platform=linux-amd64&version=latest" \
   && chmod +x cipd \
-  && ./cipd install fuchsia/sdk/linux-amd64 latest -root sdk \
-  && ./cipd install fuchsia/clang/linux-amd64 --root clang
+  && ./cipd install fuchsia/sdk/linux-amd64 84ba602e150fd736486d0907dca12cd4870aabbe -root sdk \
+  && ./cipd install fuchsia/clang/linux-amd64 git_revision:70a812b6f9a686e288468cc448144f910e361939 --root clang
